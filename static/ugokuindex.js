f056a4f2a5c4a9060a1ce965d9709f1d3534c216
{104104104} <{rz344788@yahoo.co.jp}>
2019-09-16 21:54:41 +0900

    元サイトからコピペし直してきた`

--- static/ugokuindex.js
+++ static/ugokuindex.js
@@ -1,43 +1,29 @@
-//動くカメラのjs
-
-/*
-const medias = {
-    audio: false,
-    video: {
-        facingMode: {
-            exact: "environment"
-        }
-    }
+const medias = {
+    audio: false,
+    video: true
 };
-*/
-const  medias = {
-    audio:  false,
-    video:  true
-};
-const  video  =  document.getElementById("video");
-const  canvas  =  document.getElementById("canvas");
-const  ctx  =  canvas.getContext("2d");
-const  promise  =  navigator.mediaDevices.getUserMedia(medias);
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const promise = navigator.mediaDevices.getUserMedia(medias);
 
-promise.then(successCallback)    .catch(errorCallback);
+promise.then(successCallback)
+    .catch(errorCallback);
 
-function  successCallback(stream)  {    
-    video.srcObject  =  stream;    
+function successCallback(stream) {
+    video.srcObject = stream;
     requestAnimationFrame(draw);
 }
 
-function  errorCallback(err)  {    
-    console.log(err);    
+function errorCallback(err) {
+    console.log(err);
     alert(err);
 }
 
+function draw() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.drawImage(video, 0, 0);
 
-function  draw()  {    
-    canvas.width  =  window.innerWidth;    
-    canvas.height  =  window.innerHeight;    
-    ctx.drawImage(video,  0,  0);
-
-
-        
     requestAnimationFrame(draw);
 }
