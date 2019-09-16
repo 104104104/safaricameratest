7fd8b00080eada1c51f740721d91fb7aaf4878bf
{104104104} <{rz344788@yahoo.co.jp}>
2019-09-16 20:59:59 +0900

    ボタン領域できた

--- static/index.js
+++ static/index.js
@@ -1,3 +1,5 @@
+//本番用(リアカメラ)
+/*
 const medias = {
     audio: false,
     video: {
@@ -6,30 +8,55 @@ const medias = {
         }
     }
 };
+*/
+//PCのテスト用(フロントカメラ)
+const medias = {
+    audio: false,
+    video: true
+};
+
 const video = document.getElementById("video");
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
 const promise = navigator.mediaDevices.getUserMedia(medias);
 
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
 promise.then(successCallback)
-    .catch(errorCallback);
+    .then(errorCallback);
 
 function successCallback(stream) {
     video.srcObject = stream;
     requestAnimationFrame(draw);
-}
+};
 
 function errorCallback(err) {
-    console.log(err);
-    alert(err);
-}
-
+    //alert(err);
+};
 
 function draw() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     ctx.drawImage(video, 0, 0);
 
+    //ボタンの描画
+    button();
+
+
+    /*
+    ctx.rect(0, canvas.height - canvas.height / 9, canvas.width, canvas.height / 9);
+    ctx.fillStyle = "rgba(0,0,0,0.8)";
+    ctx.fill();
+    */
+
+    //button(400, 400, 50, 50);
 
     requestAnimationFrame(draw);
-}
+};
+
+//ボタン描画
+function button() {
+    document.getElementById('mybutton').style.top = "90%";
+    document.getElementById('mybutton').style.left = 0;
+    document.getElementById('mybutton').style.width = "100%";
+    document.getElementById('mybutton').style.height = "10%";
+};
