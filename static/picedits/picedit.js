5e77af98af14c9ea31ca35816c9c9b933954b4f5
{104104104} <{rz344788@yahoo.co.jp}>
2019-09-18 22:40:23 +0900

    線引けるようになった

--- static/picedits/picedit.js
+++ static/picedits/picedit.js
@@ -10,25 +10,9 @@ img.src = "test.jpeg";
 
 ctx.fillRect(0, 0, 360, 270);
 
-ctx.fillRect(0, 0, 360, 270);
-//ここに具体的な描画内容を指定する
-ctx.lineWidth = 20;
-//新しいパスを開始する
-ctx.beginPath();
-//パスの開始座標を指定する
-ctx.moveTo(100, 20);
-//座標を指定してラインを引いていく
-ctx.lineTo(150, 100);
-ctx.lineTo(50, 100);
-//パスを閉じる（最後の座標から開始座標に向けてラインを引く）
-ctx.closePath();
-//現在のパスを輪郭表示する
-ctx.stroke();
-
-requestAnimationFrame(draw);
+draw();
 
 function draw() {
-    //videoPositionは毎フレーム更新
     videoPosition = canvassoto.getBoundingClientRect();
     //canvasの大きさ
     canvas.width = videoPosition.width;
@@ -36,24 +20,11 @@ function draw() {
     //imgの大きさ
     img.width = videoPosition.width;
     img.height = videoPosition.height;
-
-    //手前の画像表示
-    temae();
-
-    requestAnimationFrame(draw);
 };
 
-//手前の画像関連
-function temae() {
-    //画像をcanvasに設定
-    //元々の画像がスマホの写真なので、画像の縦横比が壊れる問題は、大丈夫か？
-    //だって、合成先もスマホのカメラだからな…
-    //ctx.drawImage(img, 0, 0, canvas.width / 4 * myscale, canvas.height / 4 * myscale); //400x300に縮小表示
-};
-
-
-
+//
 //ファイル読み込み関連
+//
 var file = document.getElementById('file');
 var uploadImgSrc;
 
@@ -82,6 +53,10 @@ function loadLocalImage(e) {
 // ファイルが指定された時にloadLocalImage()を実行
 file.addEventListener('change', loadLocalImage, false);
 
+//
+//ファイル読み込み関連ここまで
+//
+
 // Canvasの後ろに画像を表示する
 function imgDraw() {
     // canvas内の要素をクリアする
@@ -102,30 +77,27 @@ var drawflug = false;
 var mouseX1;
 var mouseY1;
 
+//ペンの太さのデフォルト値
+ctx.lineWidth = 10;
+//ペンのデフォルトの色は緑(透明にする部分を選択モード)
+ctx.strokeStyle = "rgba(0, 255, 0, 1.0)";
+
 //マウスの座標を取得する
 canvas.addEventListener("mousemove", function(e) {
     console.log('moving!');
     var rect = e.target.getBoundingClientRect();
     //ctx.lineWidth = document.getElementById("lineWidth").value;
-    ctx.lineWidth = 10;
-    ctx.strokeStyle = "#0000ff";
-    //ctx.globalAlpha = document.getElementById("alpha").value / 100;
-    //ctx.globalAlpha = 1;
 
     mouseX = e.clientX - rect.left;
     mouseY = e.clientY - rect.top;
-
-
-    //console.log(e);
-
 
     //クリック状態なら描画をする
     if (drawflug === true) {
         console.log('drawing!');
         ctx.beginPath();
         //console.log(mouseX, mouseY, mouseX1, mouseY1);
-        ctx.moveTo(0, 0);
-        ctx.lineTo(100, 100);
+        ctx.moveTo(mouseX1, mouseY1);
+        ctx.lineTo(mouseX, mouseY);
         ctx.lineCap = "round";
         ctx.stroke();
         mouseX1 = mouseX;
@@ -147,3 +119,41 @@ canvas.addEventListener("mousedown", function(e) {
 canvas.addEventListener("mouseup", function(e) {
     drawflug = false;
 });
+
+//ペンの太さ変え
+const smallpen = document.getElementById("smallpen");
+const mediumpen = document.getElementById("mediumpen");
+const largepen = document.getElementById("largepen");
+smallpen.onclick = function() {
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "rgba(0, 255, 0, 1.0)";
+    ctx.globalCompositeOperation = 'source-over';
+}
+mediumpen.onclick = function() {
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = "rgba(0, 255, 0, 1.0)";
+    ctx.globalCompositeOperation = 'source-over';
+}
+largepen.onclick = function() {
+        ctx.lineWidth = 30;
+        ctx.strokeStyle = "rgba(0, 255, 0, 1.0)";
+        ctx.globalCompositeOperation = 'source-over';
+    }
+    //消しゴムモード
+const erace = document.getElementById("erace");
+erace.onclick = function() {
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = "rgba(255, 255, 255, 1.0)";
+    ctx.globalCompositeOperation = 'destination-out';
+}
+
+//全消去
+const deleteall = document.getElementById("deleteall");
+deleteall.onclick = function() {
+    if (!confirm('本当に消去しますか？')) return;
+    ctx.globalCompositeOperation = 'destination-out';
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //通常ペンに戻す
+    ctx.strokeStyle = "rgba(0, 255, 0, 1.0)";
+    ctx.globalCompositeOperation = 'source-over';
+}
