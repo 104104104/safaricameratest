03faff11958af95d1f5193da15e985594c76f42e
{104104104} <{rz344788@yahoo.co.jp}>
2019-09-19 23:17:36 +0900

    最新版のpublic生成した。lightsailからgit clone してみる

--- public/index.js
+++ public/index.js
@@ -1,3 +1,9 @@
+//透明の編集を終えた画像を受け取る
+var recieveImg = location.search.substring(1);
+//console.log(recieveImg);
+
+//本番用(リアカメラ)
+/*
 const medias = {
     audio: false,
     video: {
@@ -6,43 +12,28 @@ const medias = {
         }
     }
 };
-const video = document.getElementById("video");
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const promise = navigator.mediaDevices.getUserMedia(medias);
-
-promise.then(successCallback)
-    .catch(errorCallback);
-
-function successCallback(stream) {
-    video.srcObject = stream;
-    requestAnimationFrame(draw);
-}
-
-function errorCallback(err) {
-    console.log(err);
-    alert(err);
-}
-
-function draw() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    ctx.drawImage(video, 0, 0);
-
-    requestAnimationFrame(draw);
-}
+*/
+//PCのテスト用(フロントカメラ)
 const medias = {
     audio: false,
-    video: {
-        facingMode: {
-            exact: "environment"
-        }
-    }
+    video: true
 };
+
 const video = document.getElementById("video");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const promise = navigator.mediaDevices.getUserMedia(medias);
+var videoPosition = video.getBoundingClientRect();
+
+var myscale = 1; //sliderが動いたら、ここの値を書き換える
+
+//画像オブジェクトを生成
+var img = new Image();
+//テスト用
+//img.src = "test.png";
+//本番用
+img.src = recieveImg;
+//console.log(img.src);
 
 promise.then(successCallback)
     .catch(errorCallback);
@@ -50,83 +41,153 @@ promise.then(successCallback)
 function successCallback(stream) {
     video.srcObject = stream;
     requestAnimationFrame(draw);
-}
-
-function errorCallback(err) {
-    console.log(err);
-    alert(err);
-}
-
-function draw() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    ctx.drawImage(video, 0, 0);
-
-    requestAnimationFrame(draw);
-}
-const medias = {
-    audio: false,
-    video: {
-        facingMode: {
-            exact: "environment"
-        }
-    }
 };
-const video = document.getElementById("video");
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const promise = navigator.mediaDevices.getUserMedia(medias);
-
-promise.then(successCallback)
-    .catch(errorCallback);
-
-function successCallback(stream) {
-    video.srcObject = stream;
-    requestAnimationFrame(draw);
-}
 
 function errorCallback(err) {
-    console.log(err);
     alert(err);
-}
-
-function draw() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    ctx.drawImage(video, 0, 0);
-
-    requestAnimationFrame(draw);
-}
-const medias = {
-    audio: false,
-    video: {
-        facingMode: {
-            exact: "environment"
-        }
-    }
 };
-const video = document.getElementById("video");
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const promise = navigator.mediaDevices.getUserMedia(medias);
-
-promise.then(successCallback)
-    .catch(errorCallback);
-
-function successCallback(stream) {
-    video.srcObject = stream;
-    requestAnimationFrame(draw);
-}
-
-function errorCallback(err) {
-    console.log(err);
-    alert(err);
-}
 
 function draw() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    ctx.drawImage(video, 0, 0);
+    //videoPositionは毎フレーム更新
+    videoPosition = video.getBoundingClientRect();
+    canvas.width = videoPosition.width;
+    canvas.height = videoPosition.height;
+    //ctx.drawImage(video, 0, 0);
+
+    //手前の画像表示
+    temae();
+
+    //ボタンの描画
+    button();
 
     requestAnimationFrame(draw);
+};
+
+//手前の画像関連
+function temae() {
+    //画像をcanvasに設定
+    //元々の画像がスマホの写真なので、画像の縦横比が壊れる問題は、大丈夫か？
+    //だって、合成先もスマホのカメラだからな…
+    ctx.drawImage(img, 0 + diff.x, 0 + diff.y, videoPosition.width / 4 * myscale, videoPosition.height / 4 * myscale); //400x300に縮小表示
+};
+
+const slider = document.getElementById('zoom-slider');
+slider.value = 1;
+// 倍率の最小・最大値
+slider.min = 0.01;
+slider.max = 4;
+// 粒度
+slider.step = 'any';
+
+// スライダーが動いたら拡大・縮小して再描画する
+slider.addEventListener('input', e => {
+    //console.log(myscale);
+    // 一旦クリア 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // 倍率変更
+    myscale = e.target.value;
+});
+
+//
+//ドラッグで移動するためのあれこれ
+//
+// ドラッグ状態かどうか
+let isDragging = false;
+// ドラッグ開始位置
+let start = {
+    x: 0,
+    y: 0
+};
+// ドラッグ中の位置
+let diff = {
+    x: 0,
+    y: 0
+};
+// ドラッグ終了後の位置
+let end = {
+    x: 0,
+    y: 0
 }
+canvas.addEventListener('mousedown', event => {
+    isDragging = true;
+    start.x = event.clientX;
+    start.y = event.clientY;
+});
+canvas.addEventListener('mousemove', event => {
+    //console.log(event);
+    if (isDragging) {
+        diff.x = (event.clientX - start.x) + end.x;
+        diff.y = (event.clientY - start.y) + end.y;
+    }
+});
+canvas.addEventListener('mouseup', event => {
+    isDragging = false;
+    end.x = diff.x;
+    end.y = diff.y;
+});
+
+//
+//スマホで動かすためのあれこれ
+//
+var finger = {
+    x: 0,
+    y: 0,
+    x1: 0,
+    y1: 0,
+};
+
+//タッチした瞬間座標を取得
+canvas.addEventListener("touchstart", function(e) {
+    e.preventDefault();
+    var rect = e.target.getBoundingClientRect();
+    //console.log(e.touches[0].clientX);
+    isDragging = true;
+    start.x = e.touches[0].clientX - rect.left;
+    start.y = e.touches[0].clientY - rect.top;
+});
+
+//タッチして動き出したら描画
+canvas.addEventListener("touchmove", function(e) {
+    e.preventDefault();
+    var rect = e.target.getBoundingClientRect();
+    if (isDragging) {
+        diff.x = (e.touches[0].clientX - rect.left - start.x) + end.x;
+        diff.y = (e.touches[0].clientY - rect.top - start.y) + end.y;
+    }
+});
+//タッチを離したら、移動フラグを下ろす
+canvas.addEventListener("touchend", function(e) {
+    e.preventDefault();
+    isDragging = false;
+    end.x = diff.x;
+    end.y = diff.y;
+});
+
+//
+//スマホで描画ここまで
+//
+
+
+//ボタン描画
+function button() {
+    //黒の背景
+    //videoのいちに合わせて表示する
+    //document.getElementById('mybutton').style.top = videoPosition.bottom - 70 + "px";
+    document.getElementById('mybutton').style.top = "90%";
+    document.getElementById('mybutton').style.left = 0;
+    document.getElementById('mybutton').style.width = "100%";
+    document.getElementById('mybutton').style.height = "70px";
+};
+
+
+//ボタンが押された時に、canvas→画像する部分
+var aImg = document.getElementById('aImg');
+aImg.addEventListener('click', function() {
+    //console.log('click');
+    ctx.drawImage(video, 0, 0, videoPosition.width, videoPosition.height); // canvasに関数実行時の動画のフレームを描画
+    temae();
+    var png = canvas.toDataURL();
+    aImg.href = png;
+    aImg.download = 'cancas.png'
+    aImg.click();
+});
---
+++ public/picedits/picedit.js
@@ -0,0 +1,366 @@
+const canvas = document.getElementById("canvas");
+const canvasForToumei = document.getElementById("canvasForToumei");
+const ctx = canvas.getContext("2d");
+const canvassoto = document.getElementById("canvassoto");
+
+var myscale = 4; //sliderが動いたら、ここの値を書き換える
+
+//画像オブジェクトを生成
+var img = document.getElementById("editimg");
+img.src = "test.jpeg";
+
+ctx.fillRect(0, 0, 360, 270);
+
+draw();
+
+function draw() {
+    videoPosition = canvassoto.getBoundingClientRect();
+    //canvasの大きさ
+    canvas.width = videoPosition.width;
+    canvas.height = videoPosition.height;
+    //canvasForToumeiの大きさ
+    canvasForToumei.width = videoPosition.width;
+    canvasForToumei.height = videoPosition.height;
+    //imgの大きさ
+    //もう一回imgに読み込む
+    img = document.getElementById("editimg");
+    //読み込み終わってから、縦横比を計算
+    img.onload = function() {
+        console.log(img);
+        console.log(img.width, img.height);
+        var canvasAspect = canvas.width / canvas.height;
+        var imgAspect = img.width / img.height;
+        if (imgAspect >= canvasAspect) { //画像が横長
+            img.width = canvas.width;
+            img.height = canvas.width / imgAspect;
+        } else { //画像が縦長
+            img.height = canvas.height;
+            img.width = canvas.height * imgAspect;
+            img.style.width = "auto";
+
+        }
+        /*
+        if (img.width >= img.height) {
+            img.width = canvas.width;
+            img.style.height = "auto";
+            console.log('w');
+        } else {
+            img.height = canvas.height;
+            img.style.width = "auto";
+            console.log('h');
+        }
+        */
+        //img.height = videoPosition.height;
+    }
+};
+
+//
+//ファイル読み込み関連
+//
+var file = document.getElementById('file');
+var uploadImgSrc;
+
+function loadLocalImage(e) {
+    // ファイル情報を取得
+    var fileData = e.target.files[0];
+
+    // 画像ファイル以外は処理を止める
+    if (!fileData.type.match('image.*')) {
+        alert('画像を選択してください');
+        return;
+    }
+
+    // FileReaderオブジェクトを使ってファイル読み込み
+    var reader = new FileReader();
+    // ファイル読み込みに成功したときの処理
+    reader.onload = function() {
+            // Canvasの後ろに画像を表示する
+            uploadImgSrc = reader.result;
+            img.src = uploadImgSrc;
+            draw();
+            //drawを走らせると、なぜかペンの色が変わるので、リセット
+            ctx.lineWidth = 10;
+            ctx.strokeStyle = "rgba(0, 255, 0, 1.0)";
+            ctx.globalCompositeOperation = 'source-over';
+        }
+        // ファイル読み込みを実行
+    reader.readAsDataURL(fileData);
+}
+
+// ファイルが指定された時にloadLocalImage()を実行
+file.addEventListener('change', loadLocalImage, false);
+
+//
+//ファイル読み込み関連ここまで
+//
+
+
+
+//
+//描画関連
+//
+//マウスを操作する
+var mouse = { x: 0, y: 0, x1: 0, y1: 0, color: "black" };
+var drawflug = false;
+
+var mouseX1;
+var mouseY1;
+
+//ペンの太さのデフォルト値
+ctx.lineWidth = 10;
+//ペンのデフォルトの色は緑(透明にする部分を選択モード)
+ctx.strokeStyle = "rgba(0, 255, 0, 1.0)";
+
+//マウスの座標を取得する
+canvas.addEventListener("mousemove", function(e) {
+    //console.log('moving!');
+    var rect = e.target.getBoundingClientRect();
+    //ctx.lineWidth = document.getElementById("lineWidth").value;
+
+    mouseX = e.clientX - rect.left;
+    mouseY = e.clientY - rect.top;
+
+    //クリック状態なら描画をする
+    if (drawflug === true) {
+        //console.log('drawing!');
+        ctx.beginPath();
+        //console.log(mouseX, mouseY, mouseX1, mouseY1);
+        ctx.moveTo(mouseX1, mouseY1);
+        ctx.lineTo(mouseX, mouseY);
+        ctx.lineCap = "square";
+        ctx.stroke();
+        mouseX1 = mouseX;
+        mouseY1 = mouseY;
+
+    }
+});
+
+//クリックしたら描画をOKの状態にする
+canvas.addEventListener("mousedown", function(e) {
+    drawflug = true;
+    mouseX1 = mouseX;
+    mouseY1 = mouseY;
+    undoImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    //console.log("clicked!");
+});
+
+//クリックを離したら、描画を終了する
+canvas.addEventListener("mouseup", function(e) {
+    drawflug = false;
+});
+
+//ペンの太さ変え
+const smallpen = document.getElementById("smallpen");
+const mediumpen = document.getElementById("mediumpen");
+const largepen = document.getElementById("largepen");
+smallpen.onclick = function() {
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "rgba(0, 255, 0, 1.0)";
+    ctx.globalCompositeOperation = 'source-over';
+}
+mediumpen.onclick = function() {
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = "rgba(0, 255, 0, 1.0)";
+    ctx.globalCompositeOperation = 'source-over';
+}
+largepen.onclick = function() {
+    ctx.lineWidth = 30;
+    ctx.strokeStyle = "rgba(0, 255, 0, 1.0)";
+    ctx.globalCompositeOperation = 'source-over';
+}
+
+//消しゴムモード
+const erace = document.getElementById("erace");
+erace.onclick = function() {
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = "rgba(255, 255, 255, 1.0)";
+    ctx.globalCompositeOperation = 'destination-out';
+}
+
+//全消去
+const deleteall = document.getElementById("deleteall");
+deleteall.onclick = function() {
+    if (!confirm('本当に消去しますか？')) return;
+    ctx.globalCompositeOperation = 'destination-out';
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //通常ペンに戻す
+    ctx.strokeStyle = "rgba(0, 255, 0, 1.0)";
+    ctx.globalCompositeOperation = 'source-over';
+}
+
+//
+//スマホで描画
+//
+var finger = {
+    x: 0,
+    y: 0,
+    x1: 0,
+    y1: 0,
+};
+
+//タッチした瞬間座標を取得
+canvas.addEventListener("touchstart", function(e) {
+    e.preventDefault();
+    var rect = e.target.getBoundingClientRect();
+    console.log(e.touches[0].clientX);
+    finger.x1 = e.touches[0].clientX - rect.left;
+    finger.y1 = e.touches[0].clientY - rect.top;
+});
+
+//タッチして動き出したら描画
+canvas.addEventListener("touchmove", function(e) {
+    e.preventDefault();
+    var rect = e.target.getBoundingClientRect();
+    finger.x = e.touches[0].clientX - rect.left;
+    finger.y = e.touches[0].clientY - rect.top;
+    ctx.beginPath();
+    ctx.moveTo(finger.x1, finger.y1);
+    ctx.lineTo(finger.x, finger.y);
+    ctx.lineCap = "round";
+    ctx.stroke();
+    finger.x1 = finger.x;
+    finger.y1 = finger.y;
+});
+//
+//スマホで描画ここまで
+//
+
+
+
+//
+//選択部分を透明にする部分
+//
+var f = 1;
+var f2 = 1;
+const toEditedPic = document.getElementById("toEditedPic");
+const ctxForToumei = canvasForToumei.getContext("2d");
+toEditedPic.onclick = function() {
+    // canvasForToumeiに背景の画像を書き込む
+    ctxForToumei.drawImage(document.getElementById("editimg"), 0, 0, img.width, img.height);
+    /*
+    if (img.width >= img.height) {
+        ctxForToumei.drawImage(document.getElementById("editimg"), 0, 0, videoPosition.width, videoPosition.height);
+    } else {
+        ctxForToumei.drawImage(document.getElementById("editimg"), 0, 0, videoPosition.width, videoPosition.height);
+    }
+    */
+
+    //今のcanvas(線引いたやつ)を、canvasForToumeiに書き込むためのあれこれ
+    //一旦、tempに今のcanvas(線引いたやつ)を格納
+    var temp = canvas.toDataURL();
+    //tempcanvasimgに書き込む
+    var tempcanvasimg = document.getElementById("tempcanvasimg");
+    tempcanvasimg.src = temp;
+    //tempcanvasimg → canvasForToumei
+
+    //いか、データURIが読み込み終わってからする
+    tempcanvasimg.onload = function() {
+        if (f) {
+            console.log("a");
+            //線をcanvasForToumeiに書き込む
+            ctxForToumei.drawImage(document.getElementById("tempcanvasimg"), 0, 0, videoPosition.width, videoPosition.height);
+            console.log('aa');
+            /*
+            //処理が重すぎるので、画質を落とす
+            //出来上がった線+背景画像を、またtempに書き込む
+            temp = canvasForToumei.toDataURL();
+            //tempcanvasimgに書き込む
+            tempcanvasimg = document.getElementById("tempcanvasimg");
+            tempcanvasimg.src = temp;
+            //temocanvasimgをcanvasForToumeiに戻しながら、画質を落とす
+            var neww = 400;
+            var newratio = neww / temp.width;
+            var newh = temp.height * newratio;
+            ctxForToumei.drawImage(tempcanvasimg, 0, 0, neww, newh);
+            */
+
+            //ここから、緑部分を透明にする部分
+            console.log('b');
+            toToumeiData = ctxForToumei.getImageData(0, 0, videoPosition.width, videoPosition.height);
+            //console.log(toToumeiData);
+
+            //透過用のdataを作成
+            console.log('start');
+            for (var i = 0; i < toToumeiData.data.length; i += 4) {
+                /*
+                if (i % 500 == 0) {
+                    console.log(toToumeiData.data[i], toToumeiData.data[i + 1], toToumeiData.data[i + 2], toToumeiData.data[i + 3]);
+                }
+                */
+                //console.log('a');
+                //console.log(toToumeiData[i], toToumeiData[i + 1], toToumeiData[i + 2], toToumeiData[i + 3]);
+                //0,255,0,を見つけたら、アルファチャンネルを0にする
+                /*
+                toToumeiData.data[i] = 255;
+                toToumeiData.data[i + 1] = 255;
+                toToumeiData.data[i + 2] = 255;
+                */
+                //if ((0 <= toToumeiData.data[i] && toToumeiData.data[i] <= 20) && (245 <= toToumeiData.data[i + 1] && toToumeiData.data[i + 1] <= 255) && (0 <= toToumeiData.data[i + 2] && toToumeiData.data[i + 2] <= 20)) {
+                //if ((toToumeiData.data[i] <= 20) && (230 <= toToumeiData.data[i + 1]) && (toToumeiData.data[i + 2] <= 20)) {
+                //一番緑が大きな値かつめっちゃ緑が大きい か 緑そこそこ大きくて他の色が極端に小さい
+                if (((220 <= toToumeiData.data[i + 1]) && (toToumeiData.data[i] < toToumeiData.data[i + 1]) && (toToumeiData.data[i + 2] < toToumeiData.data[i + 1])) ||
+                    ((160 <= toToumeiData.data[i + 1]) && (toToumeiData.data[i] < 10) && (toToumeiData.data[i + 2] < 10))) {
+                    toToumeiData.data[i + 3] = 0;
+                    //console.log('GET!');
+                }
+            }
+            console.log("finish");
+            //加工したデータを戻す
+            ctxForToumei.putImageData(toToumeiData, 0, 0);
+            //console.log(ctxForToumei.getImageData(0, 0, videoPosition.width, videoPosition.height).data);
+
+            var png = canvasForToumei.toDataURL();
+            document.getElementById("testimg").src = png;
+            //console.log(png);
+
+            //createObjectURLのテスト
+            //console.log(window.URL);
+            /*
+            canvasForToumei.toBlob(function(blob) {
+                console.log(URL.createObjectURL(blob));
+                console.log(blob.type);
+                dataurl = URL.createObjectURL(blob);
+
+                const reader = new FileReader();
+
+                reader.onload = function() {
+
+                    const b64 = reader.result;
+                    console.log(b64);
+
+                }
+
+                reader.readAsDataURL(blob);
+                */
+            //console.log(dataurl);
+            //window.open('/takephoto.html/?' + dataurl, '_self'); // 新しいタブを開き、ページを表示
+            /*
+            dataurl.onload = function() {
+                window.open('/takephoto.html/?' + dataurl, '_self'); // 新しいタブを開き、ページを表示
+            };*/
+            /*
+            var newImg = document.createElement("img"),
+                url = URL.createObjectURL(blob);
+
+            newImg.onload = function() {
+                // 無効化されたため、もはや blob を読み取る必要はありません。
+                URL.revokeObjectURL(url);
+            };
+
+            newImg.src = url;
+            document.body.appendChild(newImg);
+            */
+            //});
+            //objectURL = window.URL.createObjectURL(png);
+            //console.log(png);
+
+            //生成した画像を、写真撮影モードに渡す
+            window.open('/takephoto.html/?' + png, '_self'); // 新しいタブを開き、ページを表示
+            //var toTakePhoto = document.getElementById('toTakePhoto');
+            //toTakePhoto.href = 'localhost:1313/?' + png;
+
+            //f = 0;
+        }
+    }
+
+}
